40d7a7166c1f09e561d7bee1fd122824d5f7372a
Ayan <ayan2572006@gmail.com>
2024-09-08 00:52:45 +0530

    INSERT

--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,18 @@ const client = new Client({
   connectionString: "postgresql://postgres:mysecretpassword@localhost/postgres",
 });
 
-async function createUsersTable() {
+async function insertUserData(
+  username: string,
+  email: string,
+  password: string
+) {
   await client.connect();
   const result = await client.query(`
-        CREATE TABLE users3 (
-            id SERIAL PRIMARY KEY,
-            username VARCHAR(50) UNIQUE NOT NULL,
-            email VARCHAR(255) UNIQUE NOT NULL,
-            password VARCHAR(255) NOT NULL,
-            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-        );
+    INSERT INTO users2 (username, email, password)
+    VALUES ('${username}', '${email}', '${password}')
+
     `);
   console.log(result);
 }
 
-createUsersTable();
+insertUserData("johndoe", "RrZp2@example.com", "mysecretpassword");
